15a033291e747c485b2eafe4eac6d75397581c64
Nicole Copa <nicoleemilcecopa@gmail.com>
2023-11-22 19:50:17 -0300

    Comenzando ejercicioPersonajes - mismo que el anterior pero interactuando con el usuario

---
+++ ejercicioPersonajes.py
@@ -0,0 +1,49 @@
+class Personaje():
+    def __init__(self, nombre, fuerza, velocidad):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.velocidad = velocidad
+    
+    #mostrar personaje
+    def __repr__(self):
+        return f"{self.nombre} (Fuerza: {self.fuerza}, Velocidad: {self.velocidad})"
+    
+    def __add_(self, otro):
+        nuevo_nombre = self.nombre + "-" + otro.nombre
+        nueva_fuerza = round(((self.fuerza + otro.fuerza) /2) ** 1.5)
+        nueva_velocidad = round(((self.velocidad + otro.velocidad) /2) ** 1.3)
+        
+        return Personaje(nuevo_nombre, nueva_fuerza, nueva_velocidad)
+
+# ------------------------------------------------------------------------------------------ #
+
+def mostrar_personajes():
+    if not personajes:
+        print("Aún no ha creado ningun personaje.")
+        
+
+def crear_personaje():
+    nombre = str(input("Ingrese el nombre del personaje: "))
+    fuerza = float(input("Ingrese la fuerza del personaje: "))
+    velocidad = float(input("Ingrese la velocidad del personaje"))
+    
+    return Personaje(nombre, fuerza, velocidad)
+
+personajes = []
+
+while True:
+    print("\n1. Crear personaje")
+    print("2. Fusionar personaje")
+    print("3. Mostrar personajes")
+    print("4. Salir")
+    opcion = int(input("Seleccione una opción: "))
+    
+    if opcion == 1:
+        personaje_nuevo = crear_personaje()
+        personajes.append(personaje_nuevo)
+    
+    elif opcion == 2:
+        if len(personajes) < 2:
+            print("Debe tener al menos dos personajes para fusionar.")
+        else:
+            mostrar_personajes(personajes)
